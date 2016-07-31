cbcd0492adf96044ed6b6d2c48a478fb487f6786
Carl Twyman <Carl Twyman>
2016-07-31 11:13:54 -0400

    Separates website into pages, updates background and navbar

--- smoothscroll.js
+++ smoothscroll.js
@@ -31,3 +31,14 @@ a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b(
 
 // Initialize all .smoothScroll links
 jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
+
+$(function(){ /* to make sure the script runs after page load */
+
+    $('a.read_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
+
+        event.preventDefault(); /* prevent the a from changing the url */
+        $(this).parents('.item').find('.more_text').show(); /* show the .more_text span */
+
+    });
+
+});
